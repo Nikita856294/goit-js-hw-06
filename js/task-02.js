5d7b4a247e3afac3074f14c7cd11d1cf502064a6
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-16 13:40:16 +0300

    Решил 2 задачу

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+const listItems = ingredients.map((element) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = element;
+  listItem.classList.add("item");
+  return listItem;
+});
+listIngredients.append(...listItems);
