08eb25f8f01c85a1bfae769428567c5faefbf471
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-17 17:17:34 +0300

    Решил 9 задачу

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const widgetRef = document.querySelector(".widget");
 const spanRef = document.querySelector(".color");
 const buttonRef = document.querySelector(".change-color");
-buttonRef.querySelector("click", changeColor);
+const bodyRef = document.querySelector("body");
+buttonRef.addEventListener("click", changeColor);
 
 function changeColor(event) {
-  event.currentTarget;
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
 }
